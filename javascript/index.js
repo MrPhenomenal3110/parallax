c71e8d0f8bb3357a9b66d6a5a2dd74d674d1413c
Prem B. Shah <techie.prembshah@gmail.com>
2023-10-24 16:32:07 +0530

    This adds all the commponents of a basic parallax effect website.

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () => {
+
+    const parallax_elements = document.querySelectorAll(".parallax");
+
+    let xValue = 0, yValue = 0;
+
+
+    window.addEventListener("mousemove",(e) => {
+        xValue = e.clientX - window.innerWidth / 2;
+        yValue = e.clientY - window.innerHeight / 2;
+
+       parallax_elements.forEach(element => {
+            let speedx = element.dataset.speedx;
+            let speedy = element.dataset.speedy;
+
+            let finalx = (-xValue)*(speedx);
+            let finaly = (yValue)*(speedy);
+            element.style.transform = `translateX(calc(-50% + ${finalx}px)) translateY(calc(-50% + ${finaly}px))`;
+        });
+    });
+
+    
+    
+});
+parallax_elements.forEach(element => {
+    let speedx = element.dataset.speedX;
+    console.log(speedx);
+});
